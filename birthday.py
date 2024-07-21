fea90a567acbb7411982c5e0f34f406d9dd17eac
MariiaSam <kontragent@ukr.net>
2024-07-21 15:56:00 +0300

    fixed birthday + add str

--- birthday.py
+++ birthday.py
@@ -5,7 +5,10 @@ class Birthday(Field):
 
     def __init__(self, value):
         try:
-            self.value = datetime.strptime(value, '5d.%m.%Y' )
+            self.value = datetime.strptime(value, '%d.%m.%Y' )
         except ValueError:
              raise ValueError("Invalid date format. Use DD.MM.YYYY")
+        
+    def __str__(self):
+        return f'{self.value.strftime('%d.%m.%Y')}'
     
